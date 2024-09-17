b06ad6328d79de773c7d2d2df55001744eae6c8d
Alexandria <avpaul527@gmail.com>
2024-09-17 14:43:27 -0400

    adding changes to feature branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello main or master");
+        System.out.println("Hello from feature branch");
     }
 }
